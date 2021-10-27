76d5d407a38f934900af32e910b0c74c32b19581
Jordan Szubert <jordan@szubert.tk>
2021-10-27 02:50:37 +0200

    import wavelength to CIE XYZ data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use crate::color_matching_functions_data::{CIE_XYZ, min_wavelenght};
+
+mod color_matching_functions_data;
 fn main() {
-    println!("Hello, world!");
+    let max_wl_ex=min_wavelenght+CIE_XYZ.len();
+    let max_wl_inc=max_wl_ex-1;
+    println!("Wavelength {} to {} nm.",min_wavelenght,max_wl_inc);
 }
